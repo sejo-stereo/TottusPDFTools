3527b4733785f96b6752045fb99f726343c74e82
Jose Melgarejo <51570964+sejo-stereo@users.noreply.github.com>
2024-10-18 20:26:02 +0000

    add style to main

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+
 pages = [
     st.Page("01_Unir_PDF.py",title="Unir PDF",icon=":material/close_fullscreen:"),
     st.Page("02_Separar_PDF.py",title="Separar PDF",icon=":material/add:")
