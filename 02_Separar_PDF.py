85a0fdab43cd6ce6ce5e81e4bdd3943af3e516f5
Jose Melgarejo <51570964+sejo-stereo@users.noreply.github.com>
2024-10-20 22:37:36 +0000

    qol features

--- 02_Separar_PDF.py
+++ 02_Separar_PDF.py
@@ -1,62 +1,49 @@
 import streamlit as st
 import pymupdf
 import tempfile
-import zipfile
 import os
 import shutil
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-st.title("🎈 Separar PDF")
+st.title("🎈 Separar PDF",anchor=False)
 st.write(
     "Carga tu archivo PDF y procesa para separarlos."
 )
 
+rename_flag = False
 uploaded_file= st.file_uploader(label="Upload file",type=["pdf"],accept_multiple_files=False)
-rename_flag = st.checkbox("Quiere renombrar sus archivos?")
-if rename_flag:
-    #  position = st.number_input("Posición Clave",step=1,min_value=1)
-     positions = st.slider("Posiciones Clave",step=1,min_value=1,max_value=50,value=(1,1))
-    #  start_pos = positions[0]
-    #  end_pos = positions[1]
-    #  st.write(start_pos,end_pos)
-
+number_pages_split = st.number_input(label="Ingrese cantidad de páginas",min_value=1,step=1)
 
 def remove_zip():
      os.remove("Descargas.zip")
 
 def separar_pdf(uploaded_file):
+    try:
+        remove_zip()
+    except:
+         pass
     temp_folder = tempfile.TemporaryDirectory()
     doc = pymupdf.open(stream=uploaded_file.read(),filetype="pdf")
-    for i,page in enumerate(doc):
+    npages = len(doc)
+    for i in range(0,npages,number_pages_split):
+        start_page = i
+        end_page = i + number_pages_split
         temp_doc = pymupdf.open()
-        temp_doc.insert_pdf(doc,from_page=i,to_page=i)
-        if rename_flag:
-            name_words = []
-            start_pos = positions[0]
-            end_pos = positions[1]
-            text = page.get_text(sort=True)
-            words = text.split("\n")
-            for v in range(start_pos,end_pos+1):
-                key_word = words[v-1]
-                name_words.append(key_word)
-            name_words_concat = " ".join(name_words)
-            st.write(words)
-            pdf_name = f"{name_words_concat}.pdf"
-        else:
-             pdf_name = f"{uploaded_file.name}_{i+1}.pdf"
-        temp_doc.save(os.path.join(temp_folder.name,pdf_name))
-        temp_doc.close()
-
+        temp_doc.insert_pdf(doc,from_page=start_page,to_page=end_page -1)
+        if end_page + 1 > npages:
+             end_page = npages
+        temp_doc.save(os.path.join(temp_folder.name,f"{uploaded_file.name}_{start_page+1}_{end_page}.pdf"))
     shutil.make_archive("Descargas","zip",temp_folder.name)
     with open("Descargas.zip", "rb") as fp:
             btn = st.download_button(
-                label="Descargar ZIP",
+                label="Descargar archivos separados.",
                 data=fp,
                 file_name="Descargas.zip",
                 mime="application/zip",
                 on_click=remove_zip)
+            
 if uploaded_file:
     if st.button("Separar PDF"):
         separar_pdf(uploaded_file)
--- 03_Convertir_PDF.py
+++ 03_Convertir_PDF.py
@@ -14,27 +14,37 @@ st.write(
     "Carga tu archivo PDF y conviertelos a WORD."
 )
 
-uploaded_file= st.file_uploader(label="Upload file",type=["pdf"],accept_multiple_files=False)
+uploaded_files= st.file_uploader(label="Upload file",type=["pdf"],accept_multiple_files=True)
 
-def convertir_docx(file):
-    file_name = file.name
+def remove_zip():
+     os.remove("Descargas.zip")
+
+def convertir_docx(files):
     temp_folder = tempfile.TemporaryDirectory()
-    temp_pdf_name = os.path.join(temp_folder.name,"tempfile.pdf")
-    temp_docx_name = os.path.join(temp_folder.name,f"{file_name[:-4]}.docx")
-    doc = pymupdf.open(stream=file.read(),filetype="pdf")
-    doc.save(temp_pdf_name)
-    doc.close()
-    file_name = file.name
-    parse(temp_pdf_name,temp_docx_name)
+    for file in files:
+        file_name = file.name
+        temp_pdf_name = os.path.join(temp_folder.name,"tempfile.pdf")
+        temp_docx_name = os.path.join(temp_folder.name,f"{file_name[:-4]}.docx")
+        doc = pymupdf.open(stream=file.read(),filetype="pdf")
+        doc.save(temp_pdf_name)
+        doc.close()
+        parse(temp_pdf_name,temp_docx_name)
 
-    with open(temp_docx_name, "rb") as fp:
+    for file in os.listdir(temp_folder.name):
+        file_path = os.path.join(temp_folder.name,file)
+        if file_path.endswith(".pdf"):
+             os.remove(file_path)
+
+    shutil.make_archive("Descargas","zip",temp_folder.name)
+
+    with open("Descargas.zip", "rb") as fp:
             btn = st.download_button(
-                label="Descargar Word",
+                label="Descargar Word.",
                 data=fp,
-                file_name=f"{file_name[:-4]}.docx",
-                mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-                # on_click=remove_zip)
-                )
+                file_name="Descargas.zip",
+                mime="application/zip",
+                on_click=remove_zip)
 
-if st.button("Procesar"):
-    convertir_docx(uploaded_file)
+if uploaded_files:
+    if st.button("Procesar"):
+        convertir_docx(uploaded_files)
