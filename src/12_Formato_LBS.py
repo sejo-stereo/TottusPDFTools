e0712ebad704617d941e85575d4b813c2ee17de9
Jose Melgarejo <51570964+sejo-stereo@users.noreply.github.com>
2025-03-10 14:07:24 +0000

    Se agregó la opción de renombrar cada pdf a la firma.

--- src/12_Formato_LBS.py
+++ src/12_Formato_LBS.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pymupdf
 import io
 from pathlib import Path
+import zipfile
+import datetime
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
@@ -17,28 +19,45 @@ st.markdown("""
 def firmar(pdf,firma):
     doc = pymupdf.open(stream=pdf.read(),filetype="pdf")
     img_buffer = io.BytesIO(firma.read())
-    filename = Path(pdf.name).stem
-    for page in doc:
-        page.insert_image(
-            rect = (100,550,200,850),
-            # filename = firma,
-            stream = img_buffer,
-            overlay = True,
-            keep_proportion = True
-        )
-    pdf_buffer = io.BytesIO()
-    doc.save(pdf_buffer)
-    pdf_buffer.seek(0)
-    img_buffer.seek(0)
+    zip_buffer = io.BytesIO()
+
+    with zipfile.ZipFile(zip_buffer,"w",zipfile.ZIP_DEFLATED) as zip_file:
+        # filename = Path(pdf.name).stem
+        for page_number in range(len(doc)):
+            page = doc[page_number]
+            temp_doc = pymupdf.open()
+            pdf_buffer = io.BytesIO()
+
+            words = page.get_text("words",sort=True)
+            dni = None
+            for i,word in enumerate(words):
+                text = word[4]
+                if text == "DNI":
+                    dni = words[i+2][4]
+                    # print(dni)
+                    break
+            page.insert_image(
+                rect = (100,550,200,860),
+                # filename = firma,
+                stream = img_buffer,
+                overlay = True,
+                keep_proportion = True
+            )
+            temp_doc.insert_pdf(doc, from_page=page_number, to_page=page_number)
+            temp_doc.save(pdf_buffer)
+            pdf_buffer.seek(0)
+            img_buffer.seek(0)
+            timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
+            zip_file.writestr(f"{dni} Liquidación Beneficios Sociales {timestamp}.pdf",pdf_buffer.getvalue())  
+
+    zip_buffer.seek(0)
 
     st.download_button(
-        label="Descargar PDF Firmado",
-        data=pdf_buffer,
-        file_name=f"{filename}_firmado.pdf",
-        mime="application/pdf"
-    )
-
-    
+        label="Descargar PDFs Firmados",
+        data=zip_buffer,
+        file_name="Descargas.zip",
+        mime="application/zip"
+    )  
 
 uploaded_pdf = st.file_uploader("Cargar PDF",type=["pdf"],accept_multiple_files=False)
 uploaded_firma = st.file_uploader("Cargar Firma",type=["png","jpg","jpge"],accept_multiple_files=False)
